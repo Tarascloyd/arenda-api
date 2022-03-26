602dc34124c959aa82543977ff9481c28aa27d22
taras <xkits0611@gmail.com>
2022-03-26 09:01:21 +0700

    Initial commit

---
+++ src/main/java/com/taras/arenda/ArendaApplication.java
@@ -0,0 +1,13 @@
+package com.taras.arenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taras/arenda/ArendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taras.arenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
