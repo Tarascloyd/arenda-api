0de77c14e4745550d7d5cbfe19dabd5372f2c208
TarascLoyd <xkits0611@gmail.com>
2022-08-12 08:55:32 +0700

    added some test in UserControllerTest

--- src/test/java/com/taras/arenda/UserControllerTest.java
+++ src/test/java/com/taras/arenda/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.taras.arenda;
 
+import com.taras.arenda.jpa.entity.User;
 import com.taras.arenda.jpa.repository.UserRepository;
 import com.taras.arenda.ui.model.CreateUserRequestModel;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,17 +32,100 @@ public class UserControllerTest {
     @Test
     public void postUser_whenUserIsValid_receiveCreated() {
         CreateUserRequestModel user = createValidUser();
-        ResponseEntity<Object> response = testRestTemplate.postForEntity(USERS_API_V1_URL, user, Object.class);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 
     @Test
     public void postUser_whenUserIsValid_userSavedToDb() {
         CreateUserRequestModel user = createValidUser();
-        testRestTemplate.postForEntity(USERS_API_V1_URL, user, Object.class);
+        postSignup(user, Object.class);
         assertThat(userRepo.count()).isEqualTo(1);
     }
 
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDb() {
+        CreateUserRequestModel user = createValidUser();
+        postSignup(user, Object.class);
+        User savedUser = userRepo.findAll().get(0);
+        assertThat(savedUser.getEncryptedPassword()).isNotEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void postUser_whenUserHasNullFirstName_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setFirstName(null);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasNullLastName_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setLastName(null);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasNullEmail_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setEmail(null);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasNullPassword_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setPassword(null);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasPasswordWithLessThanRequired_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setPassword("123");
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasPasswordWithMoreThanRequired_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setPassword("12345678901234567");
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasFirstNameWithLessThanRequired_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setFirstName("F");
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasLastNameWithLessThanRequired_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setLastName("L");
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasInvalidEmail_receiveBadRequest() {
+        CreateUserRequestModel user = createValidUser();
+        user.setEmail("email.com");
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    private <T> ResponseEntity<T> postSignup(Object request, Class<T> response) {
+        return testRestTemplate.postForEntity(USERS_API_V1_URL, request, response);
+    }
     private CreateUserRequestModel createValidUser() {
         CreateUserRequestModel user = new CreateUserRequestModel();
         user.setFirstName("Mark");
